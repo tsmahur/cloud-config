f4773924ae3e13ea645747d8d5444de40aa41678
tsmahur <32067811+tsmahur@users.noreply.github.com>
2021-09-01 16:53:32 +0530

    cloud-config-server

---
+++ src/test/java/com/tsm/cloudconfigserver/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsm.cloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
