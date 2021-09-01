d816188342c99c7334646852a5f2fedffbffca0b
tsmahur <32067811+tsmahur@users.noreply.github.com>
2021-09-01 19:11:27 +0530

    added dependency spring-cloud-starter-config, added import in yml for configurations-server

--- src/main/java/com/tsm/cloudconfigserver/CloudConfigServerApplication.java
+++ src/main/java/com/tsm/cloudconfigserver/CloudConfigServerApplication.java
@@ -2,8 +2,12 @@ package com.tsm.cloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
 public class CloudConfigServerApplication {
 
 	public static void main(String[] args) {
